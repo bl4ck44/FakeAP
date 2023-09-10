24489f4da5ec531fa36edf7845a0f26838f67079
bl4ck44 <hackingbl4ck44@gmail.com>
2023-09-09 19:38:43 -0600

    Version 1

--- fakeAP.py
+++ fakeAP.py
@@ -32,13 +32,41 @@ interfaces = wifi.interfaces()
 if len(interfaces) == 0:
     print("No se encontraron adaptadores inalámbricos disponibles.")
 else:
-    print("\033[1m\n[+] Adaptadores inalámbricos disponibles:\033[0m")
+    print("\033[1m\n[+] Adaptadores inalámbricos disponibles:\n\033[0m")
 
     # Almacena el nombre del primer adaptador en una variable
     primer_adaptador_nombre = interfaces[0].name()
 
     for i, iface in enumerate(interfaces, start=1):
         print(f"{i}. Nombre: {iface.name()}")
+
+
+
+# Ejecuta el comando 'ip route' y captura la salida en una variable
+try:
+    resultado = subprocess.check_output(["ip", "route"])
+    resultado = resultado.decode("utf-8")
+except subprocess.CalledProcessError as e:
+    print("Error al ejecutar el comando:", e)
+    resultado = ""
+
+# Busca la línea que contiene la palabra "default" (ruta predeterminada)
+lineas = resultado.split('\n')
+adaptador = None
+
+for linea in lineas:
+    if "default" in linea:
+        partes = linea.split()
+        indice = partes.index("dev")
+        if indice < len(partes) - 1:
+            adaptador = partes[indice + 1]
+        break
+
+# Verifica si se encontró un adaptador
+if adaptador:
+    print(f"\033[1m\n[+] El adaptador de red conectado a Internet es: {adaptador}\033[0m")
+else:
+    print("\033[1m\n[+] No se pudo determinar el adaptador de red conectado a Internet.\033[0m")
 
 
 
@@ -56,7 +84,7 @@ while True:
         os.system(f"airmon-ng start {primer_adaptador_nombre}")
         os.system(f"ifconfig {primer_adaptador_nombre} up 192.168.1.1 netmask 255.255.255.0")
         os.system("route add -net 192.168.1.0 netmask 255.255.255.0 gw 192.168.1.1")
-        os.system("iptables --table nat --append POSTROUTING --out-interface enp0s3 -j MASQUERADE")
+        os.system(f"iptables --table nat --append POSTROUTING --out-interface {adaptador} -j MASQUERADE")
         os.system(f"iptables --append FORWARD --in-interface {primer_adaptador_nombre} -j ACCEPT")
         os.system("echo 1 > /proc/sys/net/ipv4/ip_forward\n")
         subprocess.Popen(['xterm', '-e', 'bash', '-c', f'hostapd hostapd.conf -i {primer_adaptador_nombre}; exec bash'])
